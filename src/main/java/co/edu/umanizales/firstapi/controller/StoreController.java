57e778171eb82253b2d4d53fcdc253e5dd5c2c57
DaniellSantana <dsantana109855@umanizales.edu.co>
2025-04-24 20:12:40 -0500

    Codigo completo ejercicio estudiante 3

--- src/main/java/co/edu/umanizales/firstapi/controller/StoreController.java
+++ src/main/java/co/edu/umanizales/firstapi/controller/StoreController.java
@@ -1,17 +1,24 @@
 package co.edu.umanizales.firstapi.controller;
 
+import co.edu.umanizales.firstapi.model.Store;
+import co.edu.umanizales.firstapi.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/store")
+@RequestMapping("stores")
 public class StoreController {
-  @GetMapping
-    public String hello(){
-        return "Hola lindos";
+
+    @Autowired
+    private StoreService storeService;
+
+    @GetMapping
+    public List<Store> listarTiendas() {
+        return storeService.obtenerTodosLosStores();
 
     }
-
-
 }
